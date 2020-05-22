b781dfec8eb1875a84d3bdf842387f12daa11e43
Alexander Nenninger <alexander@NENNIA.UBUNTU>
2020-05-22 18:55:17 +0200

    Added Operator Assembler

--- Ex5/OperatorAssembler.m
+++ Ex5/OperatorAssembler.m
@@ -1,10 +1,13 @@
 function [A] = OperatorAssembler(N)
-  printf("Not Implemented\n")
-
+  % Programmed after the numerics Lecture
+  
+  %% Make N-1 -Id_n blocks on the upper and lower diagonal
   OffDiagBlock = - speye(N)
   OffDiagTemplate =  diag(sparse(ones(N-1,1)), -1) + diag(sparse(ones(N-1,1)), 1)
   
+  % Make block diagonal matrix
   DiagBlock = 4 * speye(N, N) + diag(-ones(N-1, 1),1) + diag(-ones(N-1, 1),-1);  
   DiagTemplate = speye(N)
   
+  % Assemble A
   A = kron(DiagTemplate, DiagBlock) + kron(OffDiagTemplate, OffDiagBlock)
