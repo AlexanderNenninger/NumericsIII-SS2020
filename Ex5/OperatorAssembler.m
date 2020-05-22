912fc4dd582dd7f9ad2808800a3a0ce3afcf4d57
Alexander Nenninger <alexander@NENNIA.UBUNTU>
2020-05-22 18:50:50 +0200

    Added Operator Assembler

--- Ex5/OperatorAssembler.m
+++ Ex5/OperatorAssembler.m
@@ -1,3 +1,10 @@
 function [A] = OperatorAssembler(N)
   printf("Not Implemented\n")
-  A = speye(N)
+
+  OffDiagBlock = - speye(N)
+  OffDiagTemplate =  diag(sparse(ones(N-1,1)), -1) + diag(sparse(ones(N-1,1)), 1)
+  
+  DiagBlock = 4 * speye(N, N) + diag(-ones(N-1, 1),1) + diag(-ones(N-1, 1),-1);  
+  DiagTemplate = speye(N)
+  
+  A = kron(DiagTemplate, DiagBlock) + kron(OffDiagTemplate, OffDiagBlock)
